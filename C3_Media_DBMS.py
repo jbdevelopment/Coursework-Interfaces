98445977edd5e12baf4ebd2643a982bcd5b44790
Joel Butcher <34771@longroad.ac.uk>
2014-12-19 11:16:40 +0000

    User Interface Updates for Josh

--- C3_Media_DBMS.py
+++ C3_Media_DBMS.py
@@ -7,7 +7,7 @@ from password_reset import *
 from create_main_layout import *
 from create_new_item_layout import *
 from create_new_customer_layout import *
-#from create_loan_layout import *
+from create_new_loan_layout import *
 from create_pat_test_layout import *
 
 from display_entry_error_dialog import *
@@ -28,34 +28,42 @@ import sys
 import pdb
 import re
 
-class MainWindow(QMainWindow):
-    """Creates the main menu window where all
-    main processes will occure"""
-
+class C3MediaDBMS(QMainWindow):
+    """docstring for C3MediaDBMS"""
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("C3 Media Database Management System")
+
+        self.setWindowTitle("C3 Media Database Management Systems")
         self.resize(800,500)
-        self.password = read_password()
+        #self.icon = QIcon(QPixmap("./icon.png"))
+        #self.setWindowIcon(self.icon)
 
-        self.printer = QPrinter()
-        self.printer.setPageSize(QPrinter.Letter)
-
+        #Connection Attribute stores the database connection
+        self.connection = None
+        
+        #stacked layout
         self.stacked_layout = QStackedLayout()
 
-        self.central_widget = QWidget()
-        self.central_widget.setLayout(self.stacked_layout)
+        self.widget = QWidget()
 
-        self.setCentralWidget(self.central_widget)
+        self.widget.setLayout(self.stacked_layout)
 
-        self.main_settings()
+        #Set the central widget to the stacked layout
 
-        #self.disable_settings()
-        #self.create_login()
-        #self.enable_setting()
-        
-        self.open_connection()
-        self.create_main_menu()
+        self.setCentralWidget(self.widget)
+
+        #Add the Menu Bar and Main Settings Etc...
+        self.settings()
+
+        self.disable_actions()
+        self.password = read_password()
+        self.create_login()
+        self.enable_actions()
+
+        #add action connections
+        self.connections()
+
+        self.create_start_screen()
         self.create_new_item()
         self.create_new_customer()
         self.create_new_loan()
@@ -64,15 +72,98 @@ class MainWindow(QMainWindow):
         self.stacked_layout.setCurrentIndex(0)
 
 
-        QShortcut(QKeySequence("Ctrl+W"), self, self.close)
-
-    def main_settings(self):
+    def disable_actions(self):
         #file actions
-        self.open = QAction("Open", self)
+        self.open.setEnabled(False) 
+        self.new.setEnabled(False)
+        self.save.setEnabled(False)
+        self.Print.setEnabled(False)
+
+        #edit actions
+        self.cut.setEnabled(False)
+        self.copy.setEnabled(False)
+        self.paste.setEnabled(False)
+        self.select_all.setEnabled(False)
+
+        #item actions
+        self.add_item.setEnabled(False)
+        self.display_item.setEnabled(False)
+        self.delete_item.setEnabled(False)
+
+        #customer actions
+        self.add_customer.setEnabled(False)
+        self.display_customer.setEnabled(False)
+        self.delete_customer.setEnabled(False)
+
+        #loan actions
+        self.add_loan.setEnabled(False)
+        self.display_loan.setEnabled(False)
+        self.delete_loan.setEnabled(False)
+
+        #pat_test actions
+        self.add_pat_test.setEnabled(False)
+        self.display_pat_test.setEnabled(False)
+        self.delete_pat_test.setEnabled(False)
+
+        #help action
+        self.help.setEnabled(False)
+        self.about.setEnabled(False)
+
+        #database menu
+        self.openDatabase.setEnabled(True)
+        self.newDatabase.setEnabled(True)
+        self.closeDatabase.setEnabled(False)
+
+    def enable_actions(self):
+        #file actions
+        self.open.setEnabled(True) 
+        self.new.setEnabled(True)
+        self.save.setEnabled(True)
+        self.Print.setEnabled(True)
+
+        #edit actions
+        self.cut.setEnabled(True)
+        self.copy.setEnabled(True)
+        self.paste.setEnabled(True)
+        self.select_all.setEnabled(True)
+
+        #item actions
+        self.add_item.setEnabled(True)
+        self.display_item.setEnabled(True)
+        self.delete_item.setEnabled(True)
+
+        #customer actions
+        self.add_customer.setEnabled(True)
+        self.display_customer.setEnabled(True)
+        self.delete_customer.setEnabled(True)
+
+        #loan actions
+        self.add_loan.setEnabled(True)
+        self.display_loan.setEnabled(True)
+        self.delete_loan.setEnabled(True)
+
+        #pat_test actions
+        self.add_pat_test.setEnabled(True)
+        self.display_pat_test.setEnabled(True)
+        self.delete_pat_test.setEnabled(True)
+
+        #help action
+        self.help.setEnabled(True)
+        self.about.setEnabled(True)
+
+        #database menu
+        self.openDatabase.setEnabled(False)
+        self.newDatabase.setEnabled(False)
+        self.closeDatabase.setEnabled(True)
+
+    def settings(self):
+    	#file actions
+        self.open = QAction("Open Database", self)
         self.new = QAction("New", self)
         self.save = QAction("Save", self)
         self.Print = QAction("Print", self)
-        self.close_window = QAction("Close Window...", self)
+        self.change_password = QAction("Change Password", self)
+        self.logout_action = QAction("Logout", self)
 
         #edit actions
         self.cut = QAction("Cut", self)
@@ -104,6 +195,11 @@ class MainWindow(QMainWindow):
         self.help = QAction('Help', self)
         self.about = QAction('About', self)
 
+        #database menu
+        self.openDatabase = QAction("Open Database", self)
+        self.newDatabase = QAction("New Database", self)
+        self.closeDatabase = QAction("Close Database", self)
+
         #menubar
         self.menubar = QMenuBar()
 
@@ -113,14 +209,15 @@ class MainWindow(QMainWindow):
         self.file_menu.addAction(self.new)
         self.file_menu.addAction(self.save)
         self.file_menu.addAction(self.Print)
-        self.file_menu.addAction(self.close_window)
+        self.file_menu.addSeparator()
+        self.file_menu.addAction(self.change_password)
+        self.file_menu.addAction(self.logout_action)
 
         #file menu shortcuts
         self.open.setShortcut('Ctrl+O')
         self.new.setShortcut('Ctrl+N')
         self.save.setShortcut('Ctrl+S')
         self.Print.setShortcut('Ctrl+P')
-        self.close_window.setShortcut('Ctrl+W')
 
         #add edit menu and add actions to edit menu
         self.edit_menu = self.menubar.addMenu("Edit")
@@ -134,7 +231,6 @@ class MainWindow(QMainWindow):
         self.copy.setShortcut('Ctrl+C')
         self.paste.setShortcut('Ctrl+V')
         self.select_all.setShortcut('Ctrl+A')
-        self.close_window.setShortcut('Ctrl+W')
 
         #item menu
         self.item_menu = self.menubar.addMenu("Item")
@@ -162,22 +258,86 @@ class MainWindow(QMainWindow):
 
         #help menu
         self.help_menu = self.menubar.addMenu("Help")
-        self.help_menu.addAction(self.about)
         self.help_menu.addAction(self.help)
+        self.help_menu.addAction(self.about)
 
         #create menubar
         self.setMenuBar(self.menubar)
 
-        #connections
-        self.open.triggered.connect(self.open_connection)
-        self.close_window.triggered.connect(self.close)
+        #tool bar
+        self.toolBar = QToolBar()
+        self.toolBar.addSeparator()
 
+        #item toolbar actions
+        self.toolBar.addAction(self.add_item)
+        self.toolBar.addAction(self.display_item)
+        self.toolBar.addAction(self.delete_item)
+        self.toolBar.addSeparator()
+
+        #customer toolbar action
+        self.toolBar.addAction(self.add_customer)
+        self.toolBar.addAction(self.display_customer)
+        self.toolBar.addAction(self.delete_customer)
+        self.toolBar.addSeparator()
+
+        #loan toolbar actions
+        self.toolBar.addAction(self.add_loan)
+        self.toolBar.addAction(self.display_loan)
+        self.toolBar.addAction(self.delete_loan)
+        self.toolBar.addSeparator()
+
+        #pat test toolbar actions
+        self.toolBar.addAction(self.add_pat_test)
+        self.toolBar.addAction(self.display_pat_test)
+        self.toolBar.addAction(self.delete_pat_test)
+        self.toolBar.addSeparator()
+
+        #database toolbar actions
+        self.toolBar.addAction(self.open)
+
+        #passord and logout actions
+        self.toolBar.addAction(self.change_password)
+        self.toolBar.addAction(self.logout_action)
+        self.toolBar.setMovable(False)
+
+        self.addToolBar(Qt.LeftToolBarArea, self.toolBar)
+
+        self.setMenuBar(self.menubar)
+
+        self.statusBar = QStatusBar()
+
+        self.setStatusBar(self.statusBar)
+
+        self.connections()
+
+    def connections(self):
+        #file menu connections
         self.new.triggered.connect(self.create_new_record_select_table_dialog)
+        self.open.triggered.connect(self.open_connection)
 
+        #edit menu connections
+
+        #item menu connections
         self.add_item.triggered.connect(self.create_new_item)
+
+        #customer menu connections
         self.add_customer.triggered.connect(self.create_new_customer)
+
+        #loan menu connections
         self.add_loan.triggered.connect(self.create_new_loan)
+
+        #pat test menu connections
         self.add_pat_test.triggered.connect(self.create_new_pat_test)
+
+        #help menu connections
+        self.about.triggered.connect(self.showAboutMessageBox)
+
+
+    def showAboutMessageBox(self):
+
+        aboutText = """This application was built by Joel Butcher using Python3, PyQt4 and uses Sqlite3. \n It is for the media department of Cambridge Community Church \n It was built to enable """
+
+        QMessageBox.about(self, "About", aboutText)
 
     def create_new_record_select_table_dialog(self):
         self.select_table_dialog_box = RadioButtonWidget("Create New Record", "Please select a table", ("Item", "Customer", "Loan", "PAT-Test"))
@@ -186,23 +346,29 @@ class MainWindow(QMainWindow):
 
         if self.selected_table == 1:
             if hasattr(self, 'create_new_item'):
-                self.stacked_layout.setCurrentIndex(1)
+                self.stacked_layout.setCurrentIndex(2)
             else:
                 self.create_new_item()
         elif self.selected_table == 2:
             if hasattr(self, 'create_new_customer'):
-                self.stacked_layout.setCurrentIndex(2)
+                self.stacked_layout.setCurrentIndex(3)
             else:
                 self.create_new_customer()
         elif self.selected_table == 3:
             pass
         elif self.selected_table == 4:
             if hasattr(self, 'create_new_pat_test'):
-                self.stacked_layout.setCurrentIndex(3)
+                self.stacked_layout.setCurrentIndex(5)
             else:
                 self.create_new_pat_test()
 
     def create_login(self):
+        self.disable_actions()
+
+        #database actions
+        self.openDatabase.setEnabled(True)
+        self.newDatabase.setEnabled(True)
+
         self.login_widget = LoginWidget()
         self.login_widget.setLayout(self.login_widget.login_layout)
 
@@ -212,25 +378,33 @@ class MainWindow(QMainWindow):
 
         self.stacked_layout.addWidget(self.login_widget)
 
-    def create_main_menu(self):
-        self.main_menu_widget = MainMenuLayout()
-        self.main_menu_widget.setLayout(self.main_menu_widget.main_menu_layout)
+    def create_start_screen(self):
+        self.start_widget = QWidget()
 
-        #connections
-        self.main_menu_widget.new_record_button.clicked.connect(self.create_new_record_select_table_dialog)
-        self.main_menu_widget.logout_button.clicked.connect(self.logout)
-        self.main_menu_widget.change_password_button.clicked.connect(self.change_password_method)
-
-        self.stacked_layout.addWidget(self.main_menu_widget)
+        self.stacked_layout.addWidget(self.start_widget)
 
     def create_new_item(self):
+        self.right_widget = QWidget()
+
         self.new_item_widget = NewItemWidget()
         self.new_item_widget.setLayout(self.new_item_widget.item_layout)
 
-        self.new_item_widget.confirm_button.clicked.connect(self.close)
+        #geometry setting for widgets
+        self.right_widget.setFixedWidth(300)
+        self.new_item_widget.setFixedWidth(300)
+
+        self.new_item_widget.confirm_button.clicked.connect(self.cancel)
         self.new_item_widget.cancel_button.clicked.connect(self.cancel)
 
-        self.stacked_layout.addWidget(self.new_item_widget)
+        self.create_new_item_layout = QHBoxLayout()
+
+        self.create_new_item_layout.addWidget(self.new_item_widget)
+        self.create_new_item_layout.addWidget(self.right_widget)
+
+        self.create_new_item_widget = QWidget()
+        self.create_new_item_widget.setLayout(self.create_new_item_layout)
+
+        self.stacked_layout.addWidget(self.create_new_item_widget)
 
     def create_new_loan(self):
         pass
@@ -261,7 +435,7 @@ class MainWindow(QMainWindow):
         month = int(month)
         day = int(day)
         self.date = ("{0}/{1}/{2}".format(day,month,year))
-        print(self.date)
+        self.statusBar.showMessage(self.date)
         self.stacked_layout.setCurrentIndex(0)
 
     def create_new_customer(self):
@@ -472,14 +646,14 @@ class MainWindow(QMainWindow):
             self.error_dialog = EntryErrorDialog('a valid Email Address')
             self.error_dialog.exec_()
             valid_email = False
-
-
         
     def login(self):
         name = self.login_widget.password_entry.text()
         if name == self.password:
              allow_access = True
              self.stacked_layout.setCurrentIndex(1)
+             self.open_connection()
+             self.enable_actions
         else:
             allow_access = False
             login_error = LoginErrorDialog()
@@ -489,27 +663,43 @@ class MainWindow(QMainWindow):
         self.stacked_layout.setCurrentIndex(0)
         self.login_widget.password_entry.clear()
         self.password = read_password()
+        self.disable_actions()
 
     def change_password_method(self):
         change_password_dialog = ChangePasswordDialog(self.password)
         change_password_dialog.exec_()  
 
     def cancel(self):
-        self.stacked_layout.setCurrentIndex(0)
+        self.stacked_layout.setCurrentIndex(1)
 
     def open_connection(self):
+        if self.connection:
+            self.close_connection()
+
         path = QFileDialog.getOpenFileName()
         self.connection = SQLConnection(path)
-        ok = self.connection.open_database()
-        print("Database connection established: {0}".format(ok))
+        opened = self.connection.open_database()
 
-def main_menu_main():
-    application = QApplication(sys.argv)
-    application.setQuitOnLastWindowClosed(False)
-    window = MainWindow()
-    window.show()
-    window.raise_()
-    application.exec_()
+        if opened:
+            self.enable_actions()
+            self.statusBar.showMessage("Database has been opened.")
+
+    def close_connection(self):
+        if self.connection:
+            closed = self.connection.close_database()
+
+            if closed:
+                self.statusBar.showMessage("Database has been closed.")
+                self.disable_actions()
+                self.connection = None
+            else:
+                self.statusBar.showMessage("An error occured!")
+        else:
+            self.statusBar.showMessage("No Database to close.")
 
 if __name__ == "__main__":
-        main_menu_main()
+    app = QApplication(sys.argv)
+    window = C3MediaDBMS()
+    window.show()
+    window.raise_()
+    app.exec_()
